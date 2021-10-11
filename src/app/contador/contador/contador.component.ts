7413d29c54e6db5d8b267a3d1d79bf3ef7112c6b
Gaston Marcilese <gastonmarcilese@gmail.com>
2021-10-11 15:35:42 -0300

    listo

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -2,25 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-contador',
-    template: `
-
-        <h1> {{ titulo }} </h1>
-        <h3>La base es: <strong> {{ base }} </strong></h3>
-
-        <button (click)="sumarRestar( base )">+ {{ base }}</button>
-
-        <span> {{ numero }} </span>
-
-        <button (click)="sumarRestar( -base )">- {{ base }}</button>
-
-    `
+    templateUrl: './contador.component.html',
+    styleUrls: ['./contador.component.css']
 })
 export class ContadorComponent {
-    titulo: string = 'Contador App';
-    numero: number = 10;
-    base: number = 5;
+    titulo: string = 'ROUND ';
+    numero: number = 3;
+    cartas: number = 6;
+    round: number = 1;
 
+
+
+    
     sumarRestar(valor: number) {
         this.numero += valor;
     }
+
+    sumarRestarCartas(valor: number) {
+        this.cartas += valor;
+    }
+
+    sumarRound(valor: number) { 
+        this.round += valor;
+
+        if(this.round === 0) {
+                    this.round += 1;
+                }
+        this.sumarRestar(2);
+        this.sumarRestarCartas(3);
+    }
+
+    resetear() {
+        this.cartas = 6;
+        this.numero = 3;
+        this.round = 1;
+    }
+
+    
+
 }
